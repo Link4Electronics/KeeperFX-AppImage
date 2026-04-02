f37fbd8b5b2a83d316381b00a094341182167ca4
Link4Electronics <link4electronics@gmail.com>
2026-04-02 00:19:53 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -8,6 +8,7 @@ echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
     cmake       \
+    curl        \
     libnatpmp   \
     luajit      \
     openal      \
