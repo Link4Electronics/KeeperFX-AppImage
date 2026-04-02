01e29e71ce1c8f9e8fffa4b64b6b70dd0bd5ffbb
Link4Electronics <link4electronics@gmail.com>
2026-04-02 00:50:49 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -34,7 +34,7 @@ git clone --recursive https://github.com/dkfans/keeperfx
 mkdir -p ./AppDir/bin
 cd keeperfx
 sed -i 's/-Werror/-Wno-error/g' linux.mk
-make -f linux.mk all -j$(nproc) CFLAGS+="-Ideps/enet6/include"
+make -f linux.mk -i -j$(nproc)
 mv -v bin/keeperfx ../AppDir/bin/keeperfx.exe
 cd .. && rm -rf keeperfx
 
